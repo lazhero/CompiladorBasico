12a5c468655cf62bf77ff868f729361bf2a088e4
Alejandro Vázques O <allvaso2@gmail.com>
2021-06-06 21:41:40 -0600

    Trying to fix semantic analyzer

--- Software/LexicalAnalizer/Semantic.py
+++ Software/LexicalAnalizer/Semantic.py
@@ -1,9 +1,12 @@
 from stack import Stack
 global TS
+TS={}
 ARGUMENTS="ARGUMENTS"
+PARAMETERS="PARAMETERS"
 ASSIGNMENT="ASSIGNMENT"
 METHODCALL="METHODCALL"
 FUNCTIONCALL="FUNCTIONCALL"
+SCOPE="SCOPE"
 
 def program(AST):
     for i in AST.getChildren():
@@ -12,28 +15,47 @@ def procedure(AST):
     Scope_Stack=Stack()
     Scope_Stack.push(AST.getChildren()[0][1])
     Scope_Count=0
-    for statement in AST.getChildren()[1:]:
-        Scope_Count=statement_classifier(AST,Scope_Count)
+    process_children(AST,Scope_Count,Scope_Stack)
 
-def arguments(AST,ScopeCount,ScopeStack):
-    pass
+def scope(AST,ScopeCount,ScopeStack):
+    ScopeStack.push("SCOPE"+str(ScopeCount))
+    ScopeCount += 1
+    ScopeCount = process_children(AST,ScopeCount,ScopeStack)
+    ScopeStack.pop()
+    return ScopeCount
     
-
+def parameters(AST,ScopeCount,ScopeStack,funcName):
+    parameters=AST[1]
+    return ScopeCount
+    
 def function_call(AST,ScopeCount,ScopeStack):
     pass
-def assignment(AST,ScopeCount,ScopeStack):
-    pass
+
 def method_call(AST,ScopeCount,ScopeStack):
     pass
-def statement_classifier(AST,ScopeCount,ScopeStack):
-    StatementName=AST.getData()
-    if(StatementName==ARGUMENTS):
-        return arguments(AST,ScopeCount,ScopeStack)
+
+def assignment(AST,ScopeCount,ScopeStack):
+    pass
+
+def statement_classifier(statement,ScopeCount,ScopeStack):
+    StatementName=statement.getData()
+    if (StatementName==SCOPE):
+        return scope(statement,ScopeCount,ScopeStack)
+    if(StatementName==PARAMETERS):
+        return parameters(statement,ScopeCount,ScopeStack,0)
     if(StatementName==ASSIGNMENT):
-        return assignment(AST,ScopeCount,ScopeStack)
+        return assignment(statement,ScopeCount,ScopeStack)
     if(StatementName==METHODCALL):
-        return method_call(AST,ScopeCount,ScopeStack)
+        return method_call(statement,ScopeCount,ScopeStack)
     if(StatementName==FUNCTIONCALL):
-        return function_call(AST,ScopeCount,ScopeStack)
+        return function_call(statement,ScopeCount,ScopeStack)
     else:
         return ScopeCount
+
+def process_children(AST,Scope_Count,Scope_Stack):
+    for statement in AST.getChildren()[1:]:
+        Scope_Count=statement_classifier(statement,Scope_Count,Scope_Stack)
+    return Scope_Count
+
+def in_TS(id_tuple):
+    return id_tuple in TS.keys()
---
+++ Software/LexicalAnalizer/function_descriptor.py
@@ -0,0 +1,7 @@
+class function_descriptor:
+
+    def __init__(self,identifier,param_numbers):
+        self.identifier=identifier
+        self.param_numbers=param_numbers
+
+    
