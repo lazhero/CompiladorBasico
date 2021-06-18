1cd28cdd6af8ae34fb2ce6f28596081dd5f24d98
Brian <briwag88@hotmail.com>
2021-06-18 17:05:23 -0600

    Archivo de pruebas de Letrero

--- out.py
+++ out.py
@@ -1,22 +1,18 @@
 import sys
-sys.path.append(r"/home/lazh/TecnologicoCostaRica/QuintoSemestre/Compi/LedAnimator/LedAnimator\Hardware")
+sys.path.append(r"d:\Github Repositorios\LedAnimator\Hardware")
 from Controller_Serial import *
 def main():
 	A=[[False,False,False,True,True,False,False,False,],[False,False,True,True,True,True,False,False,],[False,True,True,False,False,True,True,False,],[True,True,False,False,False,False,True,True,],[True,True,True,True,True,True,True,True,],[True,True,True,True,True,True,True,True,],[True,True,False,False,False,False,True,True,],[True,True,False,False,False,False,True,True,],]
 	L=[[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,True,True,True,True,True,False,],[True,True,True,True,True,True,True,False,],]
 	C=[[False,False,True,True,True,True,False,False,],[False,True,True,True,True,True,True,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[True,True,False,False,False,False,False,False,],[False,True,True,True,True,True,True,False,],[False,False,True,True,True,True,False,False,],]
 	E=[[True,True,True,True,True,True,True,True,],[True,True,True,True,True,True,True,True,],[True,True,False,False,False,False,False,False,],[True,True,True,True,True,True,True,True,],[True,True,True,True,True,True,True,True,],[True,True,False,False,False,False,False,False,],[True,True,True,True,True,True,True,True,],[True,True,True,True,True,True,True,True,],]
-	pass_word(A,)
-	pass_word(L,)
-	pass_word(C,)
-	pass_word(E,)
+	SMILE()
 def pass_word(matrix,):
-	mat=matrix
+	mat=[[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],[False,False,False,False,False,False,False,False,],]
 	for var in range(0,8,1):
 		
 		PRINT_LED_X("M",0,mat,)
-		mat=		DELETE(mat,0,1,)
-		lis=[False,False,False,False,False,False,False,False,]
-		mat=		INSERT(mat,lis,1,7,)
-		DELAY(700,"Mil",)
+		mat=		DELETE(mat,0,0,)
+		lis=		matrix[var]
+		mat=		INSERT(mat,lis,0,7,)
 main()
